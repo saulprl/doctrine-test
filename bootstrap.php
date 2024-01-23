99b49f5f29b2fc18897cf7264c919ad9d5c132d4
saulprl <phasecross@hotmail.com>
2024-01-23 16:31:54 -0700

    Initial commit and basic Doctrine setup

---
+++ bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+// bootstrap.php
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Attributes
+$config = ORMSetup::createAttributeMetadataConfiguration(
+  paths: array(__DIR__."/src"),
+  isDevMode: true,
+);
+
+// Configuring the database connection
+$connection = DriverManager::getConnection([
+  "driver" => "pdo_sqlite",
+  "path" => __DIR__ . "/db.sqlite",
+], $config);
+
+// Obtaining the entity manager
+$entityManager = new EntityManager($connection, $config);
